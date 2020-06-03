384db131208d41d119c9ac317201bc811b302bdf
Hussain Arif <hussainarifkl@gmail.com>
2020-06-03 18:11:48 +0500

    Added stuff

---
+++ src/imgFetch.js
@@ -0,0 +1,59 @@
+import React,{useState} from "react"
+import  {useEffect} from "react"
+function ImgFetch (props) {
+    const [imgSrc, SetImgSrc] = useState('');
+    const [width, setWidth] = useState(window.innerWidth)
+    const [height,setHeight] = useState(window.innerHeight)
+    const [rawSource,setRawSource] = useState('')
+    const [query,setQuery] = useState(props.query);
+    const [isLoading, setIsLoading] = useState(true);
+   // let imgSrc2 = 'https://images.unsplash.com/photo-1530908295418-a12e326966ba?&w=' + width + '&h=' + height + '&fit=crop'
+    let orientation = '';
+    if(height > width) orientation ='portrait';
+    else orientation = 'landscape';
+    useEffect(()=>{ 
+       // fetch('https://api.unsplash.com/photos/random/?client_id=4N9l2Ve0azWHqAX5FdlvGISEW9gHXOmxnIspZVo2BbQ&orientation=' + orientation)
+       fetch('https://api.unsplash.com/photos/random/?client_id=4N9l2Ve0azWHqAX5FdlvGISEW9gHXOmxnIspZVo2BbQ&query=' + props.query + '&orientation=' + orientation)
+       
+      .then(res=> res.json())
+        .then(data=>  {
+         setRawSource(data.urls.raw );
+      data.urls.raw+= "fit=fill&fill=blur" +  "&w=" +width+ "&h="+(height)
+        setIsLoading(false)
+SetImgSrc(data.urls.raw);
+console.log(imgSrc);
+    })
+   
+   // imgSrc2 = imgSrc2Temp + "&w=" + width + "&h=" + height +" &fit=clip";
+    
+  },[])
+    
+    useEffect(()=> {
+SetImgSrc(rawSource + "fit=fill&fill=blur" +  "&w=" + width+ "&h="+height);
+//imgSrc2 = imgSrc2Temp + "&w=" + width + "&h=" + height;
+setImageBackground(imgSrc);
+},[height,width]);
+    
+    function update () {
+       setWidth(window.innerWidth)
+       setHeight(window.innerHeight)
+      };
+      function setImageBackground(imgSource) {
+          document.getElementById('root').style.backgroundImage ="url(" + imgSource + ")"
+          document.getElementById('root').style.backgroundSize = width +"px" + " " + height + "px";
+          console.log(imgSource);
+      }
+    window.addEventListener("resize", update);
+    return (
+    <>
+   { //setImageBackground('https://picsum.photos/' + width + '/' + height) 
+}
+  
+  { //setImageBackground(imgSrc2) 
+  }
+  {isLoading? <p>Wait</p> : setImageBackground(imgSrc)}
+        </>
+    )
+
+}
+export default ImgFetch
---
+++ src/weatherFetch.js
@@ -0,0 +1,67 @@
+import React,{useEffect,useState} from "react"
+ 
+import ImgFetch from "./imgFetch"
+function WeatherFetch (){
+    const [main,setMain] = useState('');
+    const [time,setTime] = useState('');
+    const [temp,setTemp ] = useState('Loading...');
+    const [feelsLike,setFeelsLike] = useState('')
+    const [icon,setIcon] = useState('');
+    const [day,setDay]  = useState('');
+    const days= ["Sun","Mon","Tues","Wed","Thurs","Fri","Say"]
+    const [description,setDescription] = useState('000');
+    const [query,setQuery] = useState('');
+    const [loading, setLoading] = useState(true);
+    useEffect(()=>{
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=Karachi,pk&APPID=2e236a04f5583031b3357934d5678f1c&units=metric')
+        .then(res=> res.json())
+        .then(data=> {
+            let main = data.weather[0].main;
+            setQuery(main);
+            console.log(main);
+            setMain(data.weather[0].main);
+
+            setDescription(data.weather[0].description);
+            setIcon(data.weather[0].icon);
+            setTemp(data.main.temp)
+            setFeelsLike(data.main.feels_like);
+            setLoading(false);
+            console.log(data)
+        });
+    const timerId = setInterval(()=> {
+        getTime()
+    },1000)    
+       return ()=> {
+           clearInterval(timerId);
+       }
+    },[])
+    
+    function getTime() {
+        fetch('http://worldtimeapi.org/api/timezone/Asia/Karachi')
+        .then(res=> res.json())
+        .then(data=>{
+            setTime(data.datetime.substring(11,19));
+            setDay(days[data.day_of_week])
+        })
+    }
+    return(
+        <div>
+        {loading?  <p> please wait </p> : 
+        <>
+        <ImgFetch query={query}/>
+    <header>
+    <h1 className='main'>{main}, </h1>
+    <h1 className='description'>{description}</h1>
+    <img src={" http://openweathermap.org/img/wn/" + icon + "@2x.png"}/>
+    </header>
+       <main>
+    <h1 className='time'>{time} <span className="day"> {day}</span></h1>
+  <p className="tempHolder">Real temp:  <span className="tempSpan"> {temp}  °C </span> </p>
+  <p className="feelsLikeHolder">Fees like:  <span className="feelsLikeSpan"> {parseInt(feelsLike)}  ° C </span> </p>
+    </main> 
+    </>
+}
+</div>
+    )
+}
+export default WeatherFetch;
